fc03ee04e60196b5a6d442bbeeb8274b7fdef2d2
williamwhang <whang.william@gmail.com>
2021-09-02 15:45:41 -0300

    Configurando cliente do axios

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,11 @@
 import { useLayoutEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
     useLayoutEffect (() => {
-       fetch('http://localhost:3000/api/transactions')
-       .then(response => response.json ())
-       .then(data => console.log(data))
+       api.get('transactions')
+       .then(response => console.log(response.data))
     }, []);
 
   return (
