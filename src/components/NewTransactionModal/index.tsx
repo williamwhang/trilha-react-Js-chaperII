46111fe6626b96055298fc0fcff97cdb567d641b
williamwhang <whang.william@gmail.com>
2021-09-07 14:56:05 -0300

    Funcionamento dos botões

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,8 @@ import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/closeImg.svg';
 
-import { Container, TransactionTypeContainer } from './styles';
+import { Container, TransactionTypeContainer, RadioBox } from './styles';
+import { useState } from 'react';
 
 interface NewTransactionModal{
   isOpen: boolean;
@@ -11,6 +12,8 @@ interface NewTransactionModal{
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionModal) {
+    const [type, setType] = useState('deposit');
+    
     return (
       <Modal 
         isOpen={isOpen} 
@@ -38,19 +41,23 @@ export function NewTransactionModal({ isOpen, onRequestClose}: NewTransactionMod
           />
 
           <TransactionTypeContainer>
-            <button
+            <RadioBox
               type="button"
+              onClick={() => { setType('deposit'); }}
+              isActive={type === 'deposit'}
             >
               <img src={incomeImg} alt="Entrada" />
               <span>Entrada</span>
-            </button>
+            </RadioBox>
 
-            <button
+            <RadioBox
               type="button"
+              onClick={() => { setType('withdraw'); }}
+              isActive={type === 'withdraw'}
             >
               <img src={outcomeImg} alt="Saída" />
               <span>Saída</span>
-            </button>
+            </RadioBox>
           </TransactionTypeContainer>
 
           <input
