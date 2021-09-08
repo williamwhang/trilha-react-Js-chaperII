f6dc93116e91574783de85a3dae43fec7f153924
williamwhang <whang.william@gmail.com>
2021-09-08 15:29:04 -0300

    A Context API no React

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsContext } from './TransactionsContext';
 
 import { GlobalStyle } from "./styles/global";
+
 
 Modal.setAppElement('#root');
 
@@ -20,7 +22,7 @@ export function App() {
     }
     
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenTransactionModal}/>
 
       <Dashboard />
@@ -32,7 +34,7 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
 
